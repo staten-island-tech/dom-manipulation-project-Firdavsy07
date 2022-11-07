96bf162e875ea10ae2aee3c0bf7be590000ac1bc
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2022-11-07 10:16:42 -0500

    progress

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const DOMselectors = {
+  form: document.getElementById("name"),
   text: document.querySelector("#text"),
+  grab: document.querySelectorAll(".form-example"),
 };
+
+console.log(DOMselectors.form);
+console.log(DOMselectors.grab);
+
+function
