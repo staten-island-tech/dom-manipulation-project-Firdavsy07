e48a2714eef6b34a9fc812b8291d74c0cab6ffca
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2022-11-09 10:16:12 -0500

    color change

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const DOMselectors = {
   form: document.getElementById("name"),
   text: document.querySelector("#text"),
+  box: document.getElementById("cardChild"),
   grab: document.querySelectorAll(".form-example"),
 };
 
 console.log(DOMselectors.form);
+console.log(DOMselectors.box);
 console.log(DOMselectors.grab);
 
-function
+function backgroundAndText(background) {
+  background.style.backgroundColor = "Red";
+}
+backgroundAndText(DOMselectors.box);
