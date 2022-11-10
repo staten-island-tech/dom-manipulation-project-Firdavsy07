42343e06de046e39b93a7400f3dc9caf4c6e152a
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2022-11-10 10:14:12 -0500

    as

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const DOMselectors = {
-  form: document.getElementById("name"),
-  text: document.querySelector("#text"),
-  box: document.getElementById("cardChild"),
-  grab: document.querySelectorAll(".form-example"),
-};
+//const DOMselectors = {
+//  form: document.getElementById("name"),
+//  text: document.querySelector("#cardChild"),
+//box: document.getElementById("cardChild"),
+//  grab: document.querySelectorAll(".form-example"),
+//};
 
-console.log(DOMselectors.form);
-console.log(DOMselectors.box);
-console.log(DOMselectors.grab);
+//console.log(DOMselectors.form);
+//console.log(DOMselectors.box);
+//console.log(DOMselectors.grab);
+//console.log(DOMselectors.text);
 
-function backgroundAndText(background) {
-  background.style.backgroundColor = "Red";
-}
-backgroundAndText(DOMselectors.box);
+// function backgroundAndText(background, text) {
+//  background.style.backgroundColor = "Red";
+//  text.innerHTML = "Amogus";
+//}
+//backgroundAndText(DOMselectors.box, DOMselectors.text);
+
+const inputA = document.querySelector("#inputA");
+
+inputA.enter;
