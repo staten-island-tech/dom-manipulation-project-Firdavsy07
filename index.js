dcb531367de7c69aead4b5695a86ea7b7dfd9ca9
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2022-11-14 10:16:26 -0500

    java

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-//const DOMselectors = {
-//  form: document.getElementById("name"),
-//  text: document.querySelector("#cardChild"),
-//box: document.getElementById("cardChild"),
-//  grab: document.querySelectorAll(".form-example"),
-//};
+const DOMselectors = {
+  formA: document.getElementById("inputA"),
+  formB: document.getElementById("inputB"),
+  formC: document.getElementById("inputC"),
+  text: document.querySelectorAll("#cardChild"),
+  box: document.getElementById("cardChild"),
+  grab: document.querySelectorAll(".form-example"),
+  button: document.getElementById("btn"),
+};
 
-//console.log(DOMselectors.form);
-//console.log(DOMselectors.box);
-//console.log(DOMselectors.grab);
-//console.log(DOMselectors.text);
+console.log(DOMselectors.formA);
+console.log(DOMselectors.formB);
+console.log(DOMselectors.formC);
+console.log(DOMselectors.box);
+console.log(DOMselectors.grab);
+console.log(DOMselectors.text);
+console.log(DOMselectors.button);
 
-// function backgroundAndText(background, text) {
-//  background.style.backgroundColor = "Red";
-//  text.innerHTML = "Amogus";
-//}
-//backgroundAndText(DOMselectors.box, DOMselectors.text);
+function backgroundAndText(background, text) {
+  background.style.backgroundColor = "Red";
+  text.innerHTML = "Amogus";
+}
 
-const inputA = document.querySelector("#inputA");
-
-inputA.enter;
+DOMselectors.button.addEventListener("click", function (event) {
+  event.preventDefault();
+  backgroundAndText(DOMselectors.box, DOMselectors.text);
+});
