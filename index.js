10fd93a152a8e8570094d7b1fe99db51f9e7fb17
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2022-11-15 10:15:40 -0500

    amogus

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 const DOMselectors = {
-  formA: document.getElementById("inputA"),
-  formB: document.getElementById("inputB"),
-  formC: document.getElementById("inputC"),
-  text: document.querySelectorAll("#cardChild"),
-  box: document.getElementById("cardChild"),
+  formA: document.getElementById("form-example"),
+  value: document.querySelectorAll("#inputA"),
+  text: document.querySelector("#cardChild"),
+  box: document.querySelector("#cardChild"),
   grab: document.querySelectorAll(".form-example"),
   button: document.getElementById("btn"),
 };
 
 console.log(DOMselectors.formA);
-console.log(DOMselectors.formB);
-console.log(DOMselectors.formC);
 console.log(DOMselectors.box);
 console.log(DOMselectors.grab);
 console.log(DOMselectors.text);
 console.log(DOMselectors.button);
+console.log(DOMselectors.value);
+//function backgroundAndText(background, text) {
+// background.style.backgroundColor = "Red";//
+//text.innerHTML = "Amogus";//
+//}//
 
-function backgroundAndText(background, text) {
-  background.style.backgroundColor = "Red";
-  text.innerHTML = "Amogus";
-}
+//DOMselectors.button.addEventListener("click", function (event) {
+// event.preventDefault();//
+// backgroundAndText(DOMselectors.box, DOMselectors.text);//
+//});//
 
-DOMselectors.button.addEventListener("click", function (event) {
+DOMselectors.formA.addEventListener("submit", function (event) {
   event.preventDefault();
-  backgroundAndText(DOMselectors.box, DOMselectors.text);
+
+  let input = DOMselectors.value;
+  let response = {};
+  response.name = (0).value;
+  response.home = (1).value;
 });
