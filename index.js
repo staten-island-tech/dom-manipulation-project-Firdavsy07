d1edf22d536f5c677210785e1c5c90c2a053b2a9
Firdavsy07 <113189550+Firdavsy07@users.noreply.github.com>
2022-11-22 18:19:23 -0500

    finished

--- index.js
+++ index.js
@@ -1,49 +1,40 @@
 const DOMselectors = {
   formA: document.getElementById("form-example"),
-  input: document.querySelectorAll("#inputA"),
-  submit: document.getElementById("inputB"),
-  box: document.querySelectorAll("#cardChild"),
   container: document.getElementById("cardChild"),
-  grab: document.querySelectorAll(".form-example"),
-  button: document.getElementById("btn"),
+  button: document.getElementById("submit"),
   value1: document.querySelector(".input1"),
   value2: document.querySelector(".input2"),
   value3: document.querySelector(".input3"),
-  formB: document.getElementById("formB"),
 };
+console.log(DOMselectors);
 
-DOMselectors.formA.addEventListener("submit", function (event) {
-  event.preventDefault();
+DOMselectors.button.addEventListener("click", function (card) {
+  card.preventDefault();
+  create();
+  erase();
+});
+
+function create() {
   const name = DOMselectors.value1.value;
-  console.log(name);
-  const info = {};
-  info.name = name;
-  console.log(info);
-
   const address = DOMselectors.value2.value;
-  console.log(address);
-  info.address = address;
-  console.log(info);
-
   const hobby = DOMselectors.value3.value;
-  console.log(hobby);
-  info.hobby = hobby;
-  console.log(info);
-
   DOMselectors.container.insertAdjacentHTML(
     "afterend",
     `<div id="cardGrandChild">
     <p>${name}</p>
     <p>${address}</p> 
     <p>${hobby}</p>
-    <form id="formB">
-    <button id="btn">Erase Me</button></div>
-    </form>
+    <button class="btn">Erase Me</button>
+  </div>
     `
   );
-});
-DOMselectors.button.addEventListener("click", function (event) {
-  event.preventDefault();
-  const remove = document.getElementById("cardGrandChild");
-  remove.remove();
-});
+}
+
+function erase() {
+  let remove = document.querySelectorAll(".btn");
+  remove.forEach((eachRemove) => {
+    eachRemove.addEventListener("click", (event) => {
+      event.target.parentElement.remove();
+    });
+  });
+}
